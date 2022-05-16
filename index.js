0b41777e7b61fa92095fe406c9adb1cc67cf472c
khalidmansoor0310 <khalidmansoor725@gmail.com>
2022-05-15 19:30:51 -0800

    Node js basics

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const http = require('http');
 
 
 http.createServer(function(req,res){
+    // if someone wants to use response as an html then use http header 
+    // to tell the browser that the response is html
+    res.writeHead(200,{'Content-Type':'text/html'});
     res.write("Wellcome to Node JS");
     res.end();
 }).listen(3000);
