24220b88bf8e1cc1715e198d21687b1afefff718
khalidmansoor0310 <khalidmansoor725@gmail.com>
2022-05-16 10:05:57 -0800

    Mysql with node js Api Crud

--- index.js
+++ index.js
@@ -31,6 +31,36 @@ app.post('/newProduct',(req,res)=>{
         });
 })
 
+app.put('/editProduct/:id',(req,res)=>{
+    const id = req.params.id;
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+    const category = req.body.category;
+    const brand = req.body.brand;
+    const sql  = `UPDATE products SET name = '${name}', price = '${price}', description = '${description}', category = '${category}', brand = '${brand}' WHERE id = ${id}`;
+    conn.query(sql, (err, result) => {
+        if (!err) {
+            res.send(`${result}  updated Successfully`);
+        } else {
+            console.log(err);
+        }
+    });
+})
+
+app.delete('/deleteProduct/:id',(req,res)=>{
+    const id = req.params.id;
+    const sql  = `DELETE FROM products WHERE id = ${id}`;
+    conn.query(sql, (err, result) => {
+        if (!err) {
+            res.send(`${result}  deleted Successfully`);
+        } else {
+            console.log(err);
+        }
+    });
+})
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
